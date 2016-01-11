fe596c6d064b96492f60cf704c0a36c9d2aa7764
Guanyu <dream1230518@hotmail.com>
2016-01-11 10:25:18 +0800

    Initial Commit

---
+++ DogCloud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DogCloud
+//
+//  Created by Allen on 2016/1/11.
+//  Copyright © 2016年 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
